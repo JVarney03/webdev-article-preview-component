790e8b37bd63dacb77d0613e0c3b3606ff3aca87
Jacob Varney <jakevarney03@gmail.com>
2025-11-16 20:31:25 +0000

    implemented mobile overlay toggle animation

--- src/code.js
+++ src/code.js
@@ -1,11 +1,7 @@
-const openShare = document.getElementById("open-share-button");
-const closeShare = document.getElementById("close-share-button");
+const shareToggle = document.getElementById("open-share-button");
 const shareOverlay = document.getElementById("share-overlay");
 
-openShare.addEventListener("click", () => {
+shareToggle.addEventListener("click", () => {
   shareOverlay.classList.toggle("is-active");
-});
-
-closeShare.addEventListener("click", () => {
-  shareOverlay.classList.toggle("is-active");
+  shareToggle.classList.toggle("is-active");
 });
